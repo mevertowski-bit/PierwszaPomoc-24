774d400102ffe6b5f0802b9e731ef82773d2ce0b
mevertowski-bit <m.evertowski@gmail.com>
2026-05-15 23:25:51 +0200

    Naprawa konfiguracji i upewnienie się że index jest wysłany

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,19 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx'
-
+import mdx from '@astrojs/mdx';
 import tailwindcss from '@tailwindcss/vite';
 import svelte from '@astrojs/svelte';
+import netlify from '@astrojs/netlify'; // <--- DODAJ TO
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server', // <--- DODAJ TO (obsługa serwera)
+  adapter: netlify(), // <--- DODAJ TO
   vite: {
     plugins: [tailwindcss()]
   },
   integrations: [
     mdx(),
     svelte()
-],
+  ],
 });
