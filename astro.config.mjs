8aeca0823a04aa2bc149fce91dcfe9d96d23b9e5
mevertowski-bit <m.evertowski@gmail.com>
2026-05-16 00:26:45 +0200

    Fix: Zmiana output na static w celu usuniecia bledu 500

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,12 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import tailwindcss from '@tailwindcss/vite';
 import svelte from '@astrojs/svelte';
-import netlify from '@astrojs/netlify'; // <-- 1. DODAJ TEN IMPORT
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',       // <-- 2. DODAJ TĘ LINIJKĘ
-  adapter: netlify(),     // <-- 3. DODAJ TĘ LINIJKĘ
+  output: 'static',       // <-- ZMIENIAMY NA STATIC (błędy 500 znikną)
+  adapter: netlify(),
   vite: {
     plugins: [tailwindcss()]
   },
